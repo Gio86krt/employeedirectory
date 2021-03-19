df7fd76f07fef0ff0499e29cfe7395f1f2a30e88
Giovanni <lugk4l@gmail.com>
2021-03-19 10:14:21 -0400

    date parsed, need styling and sorting

--- src/components/TableRow/TableRow.js
+++ src/components/TableRow/TableRow.js
@@ -2,17 +2,18 @@ import React from "react";
 
 function TableRow(props) {
   const { name, email, dob, picture } = props.props;
+  const birthDay = dob.date.slice(0, 10);
   // console.log("test tablerow");
   // console.log(props.props.picture.thumbnail);
   return (
     <tr>
       <th scope="row">
-        <img src={picture.thumbnail}></img>
+        <img src={picture.medium}></img>
       </th>
       <td>{name.first}</td>
       <td>{name.last}</td>
       <td>{email}</td>
-      <td>{dob.date}</td>
+      <td>{birthDay}</td>
     </tr>
   );
 }
