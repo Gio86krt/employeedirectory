72729edc827db3fcddf7aaf365200d51b834ad02
Giovanni <lugk4l@gmail.com>
2021-03-20 19:03:29 -0400

    styling

--- src/components/TBody/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import TableRow from "../TableRow/TableRow";
-
-function TBody(props) {
-  return <TableRow props={props} />;
-}
-
-export default TBody;
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,23 +3,18 @@ import React from "react";
 import TableRow from "../TableRow/TableRow";
 
 function Table(props) {
-  // console.log(props.results[0].email, "  FROM CARD");
-
-  // const obj = props.results[0];
-  // const { name, email, picture } = obj;
-  // console.log(name, email, picture.thumbnail);
   return (
     <div className="row justify-content-center">
       <div className="col-12 table">
         <table className="table table-responsive">
           <thead>
             <tr>
-              <th scope="col">Picture</th>
-              <th scope="col">First Name</th>
+              <th scope="col">OUR FACES</th>
+              <th scope="col">FIRST NAME</th>
               <th scope="col" onClick={props.sortName} id="lastName">
-                Last Name
+                LAST NAME
               </th>
-              <th scope="col">Email</th>
+              <th scope="col">EMAIL</th>
               <th scope="col" id="dob" onClick={props.sortDob}>
                 DOB
               </th>
@@ -27,7 +22,8 @@ function Table(props) {
           </thead>
           <tbody>
             {props.results.map((el) => {
-              return <TableRow props={el} />;
+              console.log(el);
+              return <TableRow key={el.id.value} props={el} />;
             })}
           </tbody>
         </table>
